b2c91d4235656d11857943bd0fd56765991e341f
RaphaelOkuyama <raphaelokuyama123@gmail.com>
2025-08-27 13:03:47 -0300

    Conexão com o backend e inicio do visual dos slides

--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -1,23 +1,33 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+import { CourseType } from "../../../services/courseService";
+import SlideCard from "../slideCard";
 
-const SlideComponent = function () {
-    return (
-        <>
-            <div>
-                <Splide
-                    options={{
-                        type: "loop",
-                        perPage: 4,
-                        perMove: 1,
-                        pagination: false,
-                    }}
-                >
-                    <SplideSlide></SplideSlide>
-                </Splide>
-            </div>
-        </>
-    );
+interface props {
+  course: CourseType[];
+}
+
+const SlideComponent = function ({ course }: props) {
+  return (
+    <>
+      <div>
+        <Splide
+          options={{
+            type: "loop",
+            perPage: 4,
+            perMove: 1,
+            pagination: false,
+          }}
+        >
+          {course?.map((course) => (
+            <SplideSlide key={course.id}>
+              <SlideCard course={course} />
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
+    </>
+  );
 };
 
 export default SlideComponent;
