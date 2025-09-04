24d847b5f5eb2bd9dc6ee37b476428a9bdcc295b
RaphaelOkuyama <raphaelokuyama123@gmail.com>
2025-09-04 12:03:27 -0300

    Criação dos elementos de formulário de senha e do seu estilo visual - Formulário de senha

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -4,8 +4,12 @@ import UserFrom from "../src/components/profile/user";
 import HeaderAuth from "../src/components/common/headerAuth";
 import { Button, Col, Container, Row } from "reactstrap";
 import Footer from "../src/components/common/footer";
+import PasswordForm from "../src/components/profile/password";
+import { useState } from "react";
 
 const UserInfo = function () {
+    const [form, setForm] = useState("userForm")
+
     return (
         <>
             <Head>
@@ -20,15 +24,25 @@ const UserInfo = function () {
                     <p className={styles.title}>Minha Conta</p>
                     <Row className="pt-3 pb-5">
                         <Col md={4} className={styles.btnColumn}>
-                            <Button outline className={styles.renderFormBtn}>
+                            <Button
+                                outline
+                                className={styles.renderFormBtn}
+                                onClick={() => {
+                                    setForm("userForm");
+                            }}>
                                 DADOS PESSOAIS
                             </Button>
-                            <Button outline className={styles.renderFormBtn}>
+                            <Button
+                                outline
+                                className={styles.renderFormBtn}
+                                onClick={() => {
+                                    setForm("passwordForm");
+                            }}>
                                 SENHA
                             </Button>
                         </Col>
                         <Col md>
-                            <UserFrom />
+                            {form === "userForm" ? <UserFrom /> : <PasswordForm />}
                         </Col>
                     </Row>
                 </Container>
